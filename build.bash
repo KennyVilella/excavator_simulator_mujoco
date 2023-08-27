6e1158d1dfd313be92d07614a6d7e8b89bb6ecbf
Kenny Vilella <kennyvilella@gmail.com>
2023-08-27 10:35:44 +0900

    [added] cloning of soil_dynamics_cpp in build.bash

--- build.bash
+++ build.bash
@@ -6,6 +6,19 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Move to the repo
 cd $SCRIPT_DIR
 
+# Copy the soil simulator if not already present
+if [ ! -d "plugin/soil/soil_dynamics_cpp" ]; then
+  cd plugin/soil/
+  git clone https://github.com/KennyVilella/soil_dynamics_cpp
+  cd $SCRIPT_DIR
+else
+  cd plugin/soil/soil_dynamics_cpp
+  if [! "git rev-parse --is-inside-work-tree"]; then
+    git clone https://github.com/KennyVilella/soil_dynamics_cpp
+  fi
+  cd $SCRIPT_DIR
+fi
+
 # Setup the CMake environment
 cmake -S . -B build -DCMAKE_C_COMPILER=clang-16 -DCMAKE_CXX_COMPILER=clang++-16
 
